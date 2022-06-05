65be1e130c6664c85c708c7042f03513195439a0
Jenneeee <jenne@cattoor.net>
2022-06-05 14:55:10 +0200

    Fixing image with gatsby-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-sass",
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "images",
-        "path": "./src/images/"
-      },
-      __key: "images"
-    }]
+    "gatsby-plugin-sass"
+  ]
 };
