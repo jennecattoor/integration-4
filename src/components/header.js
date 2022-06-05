2eed3f3a00d24bb507626eb2790b6777434f5f43
Jenneeee <jenne@cattoor.net>
2022-06-05 16:06:42 +0200

    Changing content of header

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,13 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <h1><Link className={styles.name} to='/'>Watch out</Link></h1>
+      <h1><Link className={styles.logo} to='/'>Watch out</Link></h1>
       <nav className={styles.navigation}>
         <ul className={styles.navList}>
-          <li className={styles.navItem}><Link to='/' className={styles.navLink}>Portfolio</Link></li>
-          <li className={styles.navItem}><Link to='/about' className={styles.navLink}>About</Link></li>
-          <li className={styles.navItem}><Link to='/contact' className={styles.navLink}>Contact</Link></li>
+          <li className={styles.navItem}><Link to='/' className={styles.navLink}>Home</Link></li>
+          <li className={styles.navItem}><Link to='/whatTo' className={styles.navLink}>What To</Link></li>
+          <li className={styles.navItem}><Link to='/stories' className={styles.navLink}>Stories</Link></li>
+          <li className={styles.navItem}><Link to='/experience' className={styles.navLink}>Experience</Link></li>
         </ul>
       </nav>
       <button className={styles.hamburger}>
