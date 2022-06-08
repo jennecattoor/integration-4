d6d82b402f3e8225c02cde478471a2810215cf39
Jenneeee <jenne@cattoor.net>
2022-06-08 14:37:42 +0200

    Changing font on header + started styling

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,11 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <h1><Link className={styles.logo} to='/'>UnSpiked</Link></h1>
+      <h1><Link className={styles.logo} to='/'>Unspiked</Link></h1>
       <nav>
         <ul className={styles.navList}>
           <li className={styles.navItem}><Link to='/' className={styles.navLink} activeClassName={styles.navLinkActive}>Home</Link></li>
-          <li className={styles.navItem}><Link to='/howTo' className={styles.navLink} activeClassName={styles.navLinkActive}>How To</Link></li>
+          <li className={styles.navItem}><Link to='/quiz' className={styles.navLink} activeClassName={styles.navLinkActive}>Quiz</Link></li>
           <li className={styles.navItem}><Link to='/stories' className={styles.navLink} activeClassName={styles.navLinkActive}>Stories</Link></li>
           <li className={styles.navItem}><Link to='/experience' className={styles.navLink} activeClassName={styles.navLinkActive}>Experience</Link></li>
         </ul>
--- src/pages/arduino.js
+++ src/pages/arduino.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as styles from "./arduino.module.scss";
+import Header from '../components/header'
 
 const Arduino = () => {
   return (
