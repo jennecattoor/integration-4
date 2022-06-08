483863d191a150b8c71cb3b67824fec33894ab4f
Matthias <matthiasboeckaert@gmail.com>
2022-06-08 09:27:04 +0200

    set up arduino page

---
+++ src/pages/arduino.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as styles from "./arduino.module.scss";
+
+const Arduino = () => {
+  return (
+    <>
+      <Header />
+      <p className={styles.title}>Experience page</p>
+    </>
+  );
+};
+
+export default Arduino;
