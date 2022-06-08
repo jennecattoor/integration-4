defb9735911aec0c57618b305a7a299f809f4095
Jenneeee <jenne@cattoor.net>
2022-06-08 10:20:09 +0200

    Added footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from 'gatsby'
+import * as styles from './footer.module.scss'
+
+export default function Header() {
+
+  return (
+    <footer>
+
+    </footer>
+  )
+}
