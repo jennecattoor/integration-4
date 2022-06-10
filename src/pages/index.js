a16458c35ca29d26aeef78b818befc16245783f7
Jenneeee <jenne@cattoor.net>
2022-06-10 17:05:45 +0200

    Making basics responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,17 +17,17 @@ const IndexPage = () => {
       <section className={styles.basicsWrapper}>
         <div className={styles.basics}>
           <div className={styles.verb}>
-            <h2 className={styles.basicsTitle}>Spiking</h2>
-            <p>/spaɪkɪŋ/ · verb · male</p>
+            <h2 className={styles.basicsTitle}>&#x27AB; Spiking <span>/spaikiŋ/ · verb · male</span> </h2>
+            <p></p>
             <p>'Spiking' is when someone puts alcohol or drugs into another person's drink or their body without their knowledge and/or consent.</p>
           </div>
           <div className={styles.problem}>
-            <h2 className={styles.basicsTitle}>& what’s the problem?</h2>
+            <h2 className={styles.basicsTitle}>&#x27A9; & what’s the problem?</h2>
             <p>The number of spiking victims has grown dramatically in the last year. Additionally, a new form of spiking is rapidly spreading across countries. Our goal is to inform you about the dangers of spiking and what to do if it overcomes you.</p>
           </div>
           <div className={styles.arrow}></div>
           <div className={styles.types}>
-            <h2 className={styles.basicsTitle}>types of spiking</h2>
+            <h2 className={styles.basicsTitle}>&#x27AC; types of spiking</h2>
             <ol>
               <li><p>Alcohol: A spiked drink is one that contains extra alcohol or alcohol added to a non-alcoholic beverage when you are not watching.</p></li>
               <li><p>GHB: An odorless and colorless liquid with a slightly salty taste. At a low dose you get intoxicated, which is comparable to drunkenness.</p></li>
