be7f583136638e464fccda03b408d58d45a49e76
Jenneeee <jenne@cattoor.net>
2022-06-10 10:52:27 +0200

    Adding Spiking info

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,25 @@ const IndexPage = () => {
           <button className={styles.button}>Visit our experience</button>
         </div>
       </section>
+      <section className={styles.basicsWrapper}>
+        <div>
+          <h2>Spiking</h2>
+          <p>/spaɪkɪŋ/ · verb · male</p>
+          <p>'Spiking' is when someone puts alcohol or drugs into another person's drink or their body without their knowledge and/or consent.</p>
+        </div>
+        <div>
+          <h2>& what’s the problem?</h2>
+          <p>The number of spiking victims has grown dramatically in the last year. Additionally, a new form of spiking is rapidly spreading across countries. Our goal is to inform you about the dangers of spiking and what to do if it overcomes you.</p>
+        </div>
+        <div>
+          <h2>types of spiking</h2>
+          <ol>
+            <li><p>Alcohol: A spiked drink is one that contains extra alcohol or alcohol added to a non-alcoholic beverage when you are not watching.</p></li>
+            <li><p>GHB: An odorless and colorless liquid with a slightly salty taste. At a low dose you get intoxicated, which is comparable to drunkenness.</p></li>
+            <li><p>Opiates: A prescription drug that can be fatal if combined with alcohol as the two enhance each other.</p></li>
+          </ol>
+        </div>
+      </section>
       <Footer />
     </>
   )
