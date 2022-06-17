349a0b0237cf872008fd5992f1ee0061ecbc7a39
Jenneeee <jenne@cattoor.net>
2022-06-17 20:43:06 +0200

    Redid homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,65 +10,79 @@ const IndexPage = () => {
   return (
     <>
       <Header />
-      <section className={styles.introWrapper}>
-        <h1 className={styles.title}>What the f*ck is <br />Spiking!?</h1>
-        <div className={styles.buttonWrapper}>
-          <button className={styles.button}><Link className={styles.buttonLink} to='/tips'>See all tips</Link></button>
-          <button className={styles.button}><Link className={styles.buttonLink} to='/experience'>Visit our experience</Link></button>
+      <section className={styles.header}>
+        <h1 className={styles.headerTitle}>What the f*ck is Spiking!?</h1>
+        <div className={styles.headerButtons}>
+          <Link className={styles.headerLink} to='/tips'><button className={styles.headerButton}>Prepare against spiking</button></Link>
+          <Link className={styles.headerLink} to='/experience'><button className={styles.headerButton}>Visist VR experience</button></Link>
         </div>
-        <StaticImage className={styles.pillsHeaderRight} src="../../static/images/pills-right.svg" alt="yellow pills" />
-        <StaticImage className={styles.pillsHeaderLeft} src="../../static/images/pills-left.svg" alt="yellow pills" />
       </section>
-      <section className={styles.basicsWrapper}>
-        <h2 className={styles.basicsIntro}>First, the basics</h2>
-        <div className={styles.basics}>
-          <div className={styles.verb}>
-            <h3 className={styles.basicsTitle}>&#x27AB; Spiking <span>/spaikiŋ/ · verb · male</span> </h3>
+      <section className={styles.basics}>
+        <h2 className={styles.basicsTitle}>First, the basics</h2>
+        <div className={styles.basicsGrid}>
+          <div className={styles.basicsVerb}>
+            <h3 className={styles.gridTitle}>&#x27AB; Spiking <span>/spaikiŋ/ · verb · male</span> </h3>
             <p>'Spiking' is when someone puts alcohol or drugs into another person's drink or their body without their knowledge and/or consent.</p>
           </div>
-          <div className={styles.problem}>
-            <h3 className={styles.basicsTitle}>&#x27A9; & what’s the problem?</h3>
+          <div className={styles.basicsProblem}>
+            <h3 className={styles.gridTitle}>&#x27A9; & what’s the problem?</h3>
             <p>The number of spiking victims has grown dramatically in the last year. Additionally, a new form of spiking is rapidly spreading across countries. </p>
           </div>
-          <div className={styles.arrow}></div>
-          <div className={styles.types}>
-            <h3 className={styles.basicsTitle}>&#x27AC; types of spiking</h3>
-            <ul className={styles.basicsList}>
-              <li className={styles.basicsListItem}><p><span>Alcohol:</span> A spiked drink is one that contains extra alcohol or alcohol added to a non-alcoholic beverage when you are not watching.</p></li>
-              <li className={styles.basicsListItem}><p><span>GHB:</span> An odorless and colorless liquid with a slightly salty taste. At a low dose you get intoxicated, which is comparable to drunkenness.</p></li>
-              <li className={styles.basicsListItem}><p><span>Opiates:</span> A prescription drug that can be fatal if combined with alcohol as the two enhance each other.</p></li>
+          <div className={styles.basicsArrow}></div>
+          <div className={styles.basicsTypes}>
+            <h3 className={styles.gridTitle}>&#x27AC; types of spiking</h3>
+            <ul>
+              <li className={styles.basicsLi}><p><span>Alcohol:</span> A spiked drink is one that contains extra alcohol or alcohol added to a non-alcoholic beverage when you are not watching.</p></li>
+              <li className={styles.basicsLi}><p><span>GHB:</span> An odorless and colorless liquid with a slightly salty taste. At a low dose you get intoxicated, which is comparable to drunkenness.</p></li>
+              <li className={styles.basicsLi}><p><span>Opiates:</span> A prescription drug that can be fatal if combined with alcohol as the two enhance each other.</p></li>
             </ul>
           </div>
-          <div className={styles.squares}></div>
+          <div className={styles.basicsSquares}></div>
         </div>
-        <StaticImage className={styles.pillsBasicsRight} src="../../static/images/pills-basic-right.svg" alt="pills & capsules" />
-        <StaticImage className={styles.pillBasicsLeft} src="../../static/images/pill-basic-left.svg" alt="pill" />
       </section>
       <section className={styles.tips}>
-        <h3 className={styles.tipsTitle}>Prepare yourself and read our <span>Tips</span></h3>
-        <div className={styles.tipsButton}>
-          <button className={styles.buttonLarge}><Link className={styles.buttonLink} to='/tips'>Read the tips</Link></button>
-        </div>
+        <h2 className={styles.tipsTitle}>Prepare yourself and read our <span>tips</span></h2>
+        <Link className={styles.tipsLink} to='/tips'><button className={styles.tipsButton}>Read the tips</button></Link>
       </section>
       <section className={styles.quote}>
-        <p className={styles.quoteText}>"I was very <span>confused</span> and felt like I could not breathe" - Jane</p>
+        <p className={styles.quoteText}>"I was very <span>confused</span> and felt like I could not breathe." - Jane</p>
+      </section>
+      <section className={styles.stories}>
+        <div className={styles.storiesText}>
+          <h2 className={styles.storiesTitle}>Stories</h2>
+          <p>Click and read how these brave persons share their story about unwanted intoxication.</p>
+        </div>
+        <div className={styles.storiesGrid}>
+          <div className={styles.story}>
+            <p>Rianne &#10142;</p>
+            <StaticImage src="../../static/images/rianne-s.jpg" alt="Rianne" />
+          </div>
+          <div className={styles.story}>
+            <p>Jane &#10142;</p>
+            <StaticImage src="../../static/images/jane-s.jpg" alt="Jane" />
+          </div>
+          <div className={styles.story}>
+            <p>Noa &#10142;</p>
+            <StaticImage src="../../static/images/noa-s.jpg" alt="Noa" />
+          </div>
+        </div>
       </section>
       <section className={styles.experience}>
         <h3 className={styles.experienceTitle}>The vr experience</h3>
-        <div className={styles.infoWrapper}>
+        <div className={styles.experienceWrapper}>
           <div className={styles.experienceInfo}>
-            <p>
-              To experience what it's like to be spiked, we've made a VR experience. You will be immersed in a powerful story and feel the effects of getting spiked. Each week, we change our location in Rotterdam, so stop by and check it out!
-            </p>
-            <h4 className={styles.experienceLocation}>Current location:</h4>
+            <p>To experience what it's like to be spiked, we've made a VR experience. You will be immersed in a powerful story and feel the effects of getting spiked. Each week, we change our location in Rotterdam, so stop by and check it out!</p>
+            <h3 className={styles.experienceLocation}>Current location: </h3>
             <p>Witte de Withstraat, Rotterdam</p>
           </div>
           <div className={styles.experienceSquares}></div>
           <div className={styles.experienceMap}>
-            <StaticImage className={styles.pillBasicsLeft} src="../../static/images/map.png" alt="map of Rotterdam" />
+            <StaticImage src="../../static/images/map.png" alt="map of Rotterdam" />
           </div>
         </div>
-        <button className={styles.buttonLarge}><Link className={styles.buttonLink} to='/experience'>Visit our experience</Link></button>
+        <div className={styles.experienceLink}>
+          <Link to='/experience'><button className={styles.experienceButton}>See upcomming locations</button></Link>
+        </div>
       </section>
       <Footer />
     </>
